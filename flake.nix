1c2887d8b9405aacd80d8228473c0960381f4531
michaelBelsanti <quasigod-io@protonmail.com>
2023-04-18 20:37:43 -0400

    Added overlay flake output

--- flake.nix
+++ flake.nix
@@ -17,5 +17,6 @@
       packages = forAllSystems (system: import ./default.nix {
         pkgs = import nixpkgs { inherit system; };
       });
+      overlays.default = import ./overlay.nix;
     };
 }
